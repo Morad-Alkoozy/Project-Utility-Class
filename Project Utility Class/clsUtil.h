16606ac2e2e6df3cb0a53c07e3bd122c162b3230
Morad Alkoozy <mooradalkoozy@gmail.com>
2026-03-06 17:46:52 +0300

    Update clsUtil.h

--- Project Utility Class/clsUtil.h
+++ Project Utility Class/clsUtil.h
@@ -1,5 +1,4 @@
-
-#pragma once
+pragma once
 #include <iostream>
 #include <string>
 #include "clsDate.h"
@@ -215,7 +214,74 @@ public:
 
     }
 
-    static string  EncryptText(string Text, short EncryptionKey)
+    static string NumberToText(int Number)
+    {
+
+        if (Number == 0)
+        {
+            return "";
+        }
+
+        if (Number >= 1 && Number <= 19)
+        {
+            string arr[] = { "", "One","Two","Three","Four","Five","Six","Seven",
+        "Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen",
+          "Fifteen","Sixteen","Seventeen","Eighteen","Nineteen" };
+
+            return  arr[Number] + " ";
+
+        }
+
+        if (Number >= 20 && Number <= 99)
+        {
+            string arr[] = { "","","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety" };
+            return  arr[Number / 10] + " " + NumberToText(Number % 10);
+        }
+
+        if (Number >= 100 && Number <= 199)
+        {
+            return  "One Hundred " + NumberToText(Number % 100);
+        }
+
+        if (Number >= 200 && Number <= 999)
+        {
+            return   NumberToText(Number / 100) + "Hundreds " + NumberToText(Number % 100);
+        }
+
+        if (Number >= 1000 && Number <= 1999)
+        {
+            return  "One Thousand " + NumberToText(Number % 1000);
+        }
+
+        if (Number >= 2000 && Number <= 999999)
+        {
+            return   NumberToText(Number / 1000) + "Thousands " + NumberToText(Number % 1000);
+        }
+
+        if (Number >= 1000000 && Number <= 1999999)
+        {
+            return  "One Million " + NumberToText(Number % 1000000);
+        }
+
+        if (Number >= 2000000 && Number <= 999999999)
+        {
+            return   NumberToText(Number / 1000000) + "Millions " + NumberToText(Number % 1000000);
+        }
+
+        if (Number >= 1000000000 && Number <= 1999999999)
+        {
+            return  "One Billion " + NumberToText(Number % 1000000000);
+        }
+        else
+        {
+            return   NumberToText(Number / 1000000000) + "Billions " + NumberToText(Number % 1000000000);
+        }
+
+
+    }
+
+
+    static string  EncryptText(string Text, short EncryptionKey = 2)
     {
 
         for (int i = 0; i <= Text.length(); i++)
@@ -229,7 +295,7 @@ public:
 
     }
 
-    static string  DecryptText(string Text, short EncryptionKey)
+    static string  DecryptText(string Text, short EncryptionKey = 2)
     {
 
         for (int i = 0; i <= Text.length(); i++)
@@ -241,6 +307,7 @@ public:
         return Text;
 
     }
+
 
 };
 
